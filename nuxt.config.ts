a3ab4484421e15327326e19b4f4f0fe52e82e5ea
Willi Woldt <willi.woldt@ueberbit.de>
2022-10-03 23:45:13 +0200

    add base path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,15 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+
+const { GITHUB_REPOSITORY } = process.env
+
 export default defineNuxtConfig({
   modules: ['@nuxt/content'],
   runtimeConfig: {
     public: {
       githubRepository: ''
-    }
+    },
   },
+  app: {
+    baseURL: GITHUB_REPOSITORY ? GITHUB_REPOSITORY.split('/').at(-1) : '/',
+  }
 })
