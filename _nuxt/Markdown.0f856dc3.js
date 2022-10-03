1563646d3eb211666abb857851545044d6bec533
woldtwerk <32794633+woldtwerk@users.noreply.github.com>
2022-10-03 21:20:28 +0000

    Deploying to gh-pages from @ woldtwerk/nuxt-netlifycms@6f7822c9362759843d39c045944dba7cd0004078 🚀

---
+++ _nuxt/Markdown.0f856dc3.js
@@ -0,0 +1,1 @@
+import r from"./ContentSlot.43dde66c.js";import{a as o,b as u,y as f,G as c}from"./entry.772d1043.js";const i=o({name:"Markdown",extends:r,setup(t){const{parent:e}=c(),{between:n,default:a}=u(),s=f(()=>typeof t.unwrap=="string"?t.unwrap.split(" "):["*"]);return{fallbackSlot:a,tags:s,between:n,parent:e}}});export{i as default};
---
+++ _nuxt/_...slug_.006f2002.js
@@ -0,0 +1,1 @@
+import{H as n,o as c,v as o,N as _,O as t}from"./entry.772d1043.js";const s={};function a(r,f){const e=t;return c(),o("main",null,[_(e)])}const m=n(s,[["render",a]]);export{m as default};
