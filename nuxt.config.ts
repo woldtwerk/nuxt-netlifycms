d9ad2c7625f5b1550b983e8f7765f7ca3f00d1fd
Willi Woldt <willi.woldt@ueberbit.de>
2022-10-06 23:40:08 +0200

    feat: prerender routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,35 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 
+import { globby } from 'globby'
+
 const { GITHUB_REPOSITORY, GITHUB_REF_NAME } = process.env
 
-export default defineNuxtConfig({
-  modules: ['@nuxt/content'],
-  runtimeConfig: {
-    public: {
-      githubRepository: GITHUB_REPOSITORY || '',
-      githubRefName: GITHUB_REF_NAME || 'main'
+export default async () => {
+  const paths = await globby(['./content/**/*'])
+  const routes = paths.map(path => {
+    if (path.match(/\/_/)) return
+    return path
+      .replace(/^\.\/content/, '')
+      .replace(/\.md$/, '')
+      .replace(/index$/, '')
+  })
+
+  return defineNuxtConfig({
+    modules: ['@nuxt/content'],
+    runtimeConfig: {
+      public: {
+        githubRepository: GITHUB_REPOSITORY || '',
+        githubRefName: GITHUB_REF_NAME || 'main'
+      },
     },
-  },
-  app: {
-    baseURL: GITHUB_REPOSITORY ? `/${GITHUB_REPOSITORY.split('/').at(-1)}` : '/',
-  },
-  experimental: {
-    payloadExtraction: false
-  }
-})
+    app: {
+      baseURL: GITHUB_REPOSITORY ? `/${GITHUB_REPOSITORY.split('/').at(-1)}` : '/',
+    },
+    generate: {
+      routes
+    },
+    experimental: {
+      payloadExtraction: false
+    }
+  })
+}
